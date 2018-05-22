2fe04fba2209f05236eaa95acc8311636923194c
COSOL <c.deguzmanosol@gmail.com>
2018-05-22 11:57:58 +0200

    Updated notes

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleApp2
@@ -19,34 +20,38 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
 
+         //pre-defineret Dictonary vednavn katalog
+         katalog.Add("æble", 5);
+         katalog.Add("banan", 10.10);
+         katalog.Add("pære", 6.9);
+         katalog.Add("melon", 10.89);
+         katalog.Add("galia", 20.21);
+        
+         string kpenge = "";
 
-            katalog.Add("æble", 5);
-            katalog.Add("banan", 10.10);
-            katalog.Add("pære", 6.9);
-            katalog.Add("melon", 10.89);
-            katalog.Add("galia", 20.21);
+         Console.Write("Indtast kundens penge: ");
+         kpenge = Console.ReadLine();
+         
+         double dkpenge = 0;
+         dkpenge = Convert.ToDouble(kpenge);
+         //Console.Write("Invalid indtastning, prøv igen: ");
 
-            string kpenge = "";
+         Console.WriteLine("Du kan vælge imellem: ");
 
-            Console.WriteLine("Indtast kundens penge: ");
-            kpenge = Console.ReadLine();
-
-            double dkpenge = 0;
-
-            dkpenge = Convert.ToDouble(kpenge);
-
-            Console.WriteLine("Du kan vælge imellem: ");
-
-            foreach (var vare in katalog)
+         //Indsætter valgte vare i variablen vare, fra katalog
+         foreach (var vare in katalog)
             {
                 Console.WriteLine($"{vare.Key}\t{vare.Value}");
             }
 
-            Console.Write("Afslut med exit, indtast vare: ");
+         Console.Write("Afslut med exit, indtast vare: ");
 
-            string newline = "";
-            while ((newline = Console.ReadLine()) != "exit")
+         //Hvis der skrives exit, slutter den while loopen
+         string newline = "";
+         while ((newline = Console.ReadLine()) != "exit")
             {
+                //Her ligges vare i listen valgtevare og der tjekkes samtidig om varen eksisterer i katalog
+                // While loop i If loop
                 if (katalog.ContainsKey(newline))
                 {
                     valgtevare.Add(new KeyValuePair<string, double>(newline, katalog[newline]));
@@ -56,30 +61,30 @@ namespace ConsoleApp2
                     Console.Write("Ugyldig vare, indtast igen: ");
             }
 
-            Console.WriteLine("Kvittering: ");
-
-            double totalt = 0;
-
-            foreach (var vare in valgtevare)
+         Console.WriteLine("Kvittering: ");
+           
+         //For hver vare der blev skrevet ind i valtevare listen, blibver udskrevet, hernæst bliver summen samlet
+         double totalt = 0;
+         foreach (var vare in valgtevare)
             {
                 Console.WriteLine($"{vare.Key}\t{vare.Value}");
                 totalt = totalt + vare.Value;
             }
 
-            double tpenge = dkpenge - totalt;
+         double tpenge = dkpenge - totalt;
 
-            string totalt1 = totalt.ToString("C");
-            string tpenge1 = tpenge.ToString("C");
+         string totalt1 = totalt.ToString("C");
+         string tpenge1 = tpenge.ToString("C");
 
-            string tpenge2 = string.Format("{0:0.0}", tpenge);
+         string tpenge2 = string.Format("{0:0.0}", tpenge);
 
 
-            Console.WriteLine(DateTime.Now);
-            Console.WriteLine($"Total:\t{totalt1}");
-            Console.WriteLine($"Retur:\t{tpenge1}");
-            Console.WriteLine($"Udbetalt:\t{tpenge2}");
+         Console.WriteLine(DateTime.Now);
+         Console.WriteLine($"Total:\t{totalt1}");
+         Console.WriteLine($"Retur:\t{tpenge1}");
+         Console.WriteLine($"Udbetalt:\t{tpenge2}");
 
-            Console.Read();
+         Console.ReadKey();
         }
     }
 }
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal = 
+            Console.Write("Indtast tal: ");
+
+
+            Regex nr = new Regex(@"^\d$");
+            if (Regex.IsMatch(newtal))
+            {
+                Console.WriteLine($"Du har indtastet: {tal}");
+            }
+
+            else
+            {
+                Console.Write("Forkert indtastning, prøv igen: ");
+            }
+        }
+    }
+}
